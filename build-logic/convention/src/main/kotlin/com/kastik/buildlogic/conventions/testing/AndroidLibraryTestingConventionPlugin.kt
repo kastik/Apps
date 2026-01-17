6ca29415a0aad156a2754725f7b0c0bff75d543c
kastik <kostas21422112@gmail.com>
2026-01-17 11:00:10 +0200

    [IEE-38] General improvements - Refined Fake DataSources - Refined Test Data - Created Tests for more cases - Converted assertions to Google Truth

--- build-logic/convention/src/main/kotlin/com/kastik/buildlogic/conventions/testing/AndroidLibraryTestingConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/kastik/buildlogic/conventions/testing/AndroidLibraryTestingConventionPlugin.kt
@@ -3,9 +3,10 @@ package com.kastik.buildlogic.conventions.testing
 import com.kastik.buildlogic.conventions.extensions.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
 
-//TODO Find a way to configure power assert here
 class AndroidLibraryTestingConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         with(project) {
@@ -17,7 +18,11 @@ class AndroidLibraryTestingConventionPlugin : Plugin<Project> {
                     add("testImplementation", libs.findLibrary("kotlinx-coroutines-test").get())
                     add("testImplementation", libs.findLibrary("androidx-junit").get())
                     add("testImplementation", libs.findLibrary("androidx-test-core").get())
+                    add("testImplementation", libs.findLibrary("google-truth").get())
                 }
+            }
+            tasks.withType<Test>().configureEach {
+                jvmArgs("-XX:+EnableDynamicAgentLoading")
             }
         }
     }
---
+++ core/database/src/test/kotlin/com/kastik/apps/core/database/converters/SortTypeConverterTest.kt
@@ -0,0 +1,29 @@
+package com.kastik.apps.core.database.converters
+
+import com.google.common.truth.Truth.assertThat
+import com.kastik.apps.core.model.user.SortType
+import org.junit.Test
+
+
+class SortTypeConverterTest {
+
+    private val converter = SortTypeConverter()
+
+    @Test
+    fun mapsSortTypeToName() {
+        SortType.entries.forEach { type ->
+            val result = converter.fromSortType(type)
+            assertThat(result).isEqualTo(type.name)
+        }
+    }
+
+
+    @Test
+    fun mapsNameToSortType() {
+        SortType.entries.forEach { type ->
+            val result = converter.toSortType(type.name)
+            assertThat(result).isEqualTo(type)
+        }
+    }
+}
+
